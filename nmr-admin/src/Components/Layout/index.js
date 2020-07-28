7ce2802a55b890e7464c2ef35d4c0c19d4bc0ce3
Bikash888 <bikashdulal150@gmail.com>
2020-07-28 10:43:23 +0545

    sidebar added

--- nmr-admin/src/Components/Layout/index.js
+++ nmr-admin/src/Components/Layout/index.js
@@ -14,21 +14,39 @@ const AppLayout = () => {
 
   return (
     <Layout>
-      <Sider>
-        <div className="logo" />
-        <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
-          </Menu.Item>
-        </Menu>
-      </Sider>
-    </Layout>
+        <Sider trigger={null} collapsible collapsed={false}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(true ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+             
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
   );
 };
 export default AppLayout;
